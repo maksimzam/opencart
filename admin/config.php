6d916528d5aa64fdd031546e31f8f285454cd1a1
Maksim Zamkovsky <zamkovsky@gmail.com>
2020-09-02 16:39:52 +0300

    opencart first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/opencart/admin/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/opencart/system/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/opencart/image/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/opencart/admin/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/opencart/admin/view/template/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/opencart/system/config/');
+define('DIR_CACHE', '/Library/WebServer/Documents/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/opencart/system/storage/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/Library/WebServer/Documents/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/Library/WebServer/Documents/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/Library/WebServer/Documents/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/opencart/catalog/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/opencart/system/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/opencart/image/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/opencart/system/config/');
+define('DIR_CACHE', '/Library/WebServer/Documents/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/opencart/system/storage/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/Library/WebServer/Documents/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/Library/WebServer/Documents/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '12345');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
